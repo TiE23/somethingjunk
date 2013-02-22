761056fee030560d1dd425b9c7b3ba8c4588d7c4
Kyle Geib <kyleg@accretivetg.com>
2013-02-22 13:37:25 -0800

    Reverted from threading idea... May want to come back to it later but for now it'll have to wait.

--- CPython/Blipper/menu.py
+++ CPython/Blipper/menu.py
@@ -7,7 +7,6 @@ Created on Feb 21, 2013
 import util # utility module
 
 import sys
-import threading
 from time import sleep
 
 # GUI system
@@ -21,75 +20,48 @@ from System import InvalidOperationException
 
 
 class MainWindowMenu(QtGui.QMainWindow):
-    def __init__(self):
-        
-        
+    def __init__(self): 
         QtGui.QMainWindow.__init__(self)
         
         uic.loadUi(util.buildResPath('ui/blipper.ui'), self)   # Load window UI
         self.actionGo.triggered.connect(self.go)
-        self.actionHalt.triggered.connect(self.halt)
-        
+
+        self.halt = False
         self.progressBarCycles.hide()
-        
-        
-        
-        print "__init__"
     
     def go(self):
-        self.pcThread = performanceCounterThread(self)
-        self.pcThread.runPerformanceCounter()
-
-    def halt(self):
-        if self.pcThread is not None:
-            print "Hit the Halt button!"
-            self.pcThread.halt = True
-        else:
-            print "Nothing running now!"
+        self.runPerformanceCounter()
         
     def closeEvent(self, event):
         print "Closing!"
         
-        
-        
-        
-class performanceCounterThread(threading.Thread):
-    def __init__(self, parent):
-        threading.Thread.__init__(self)
-        self.parent = parent
-        self.halt = False
     
     def runPerformanceCounter(self):
         self.fromOptions()
         
         try:
-            self.parent.progressBarCycles.show()
+            self.progressBarCycles.show()
             pc = PerformanceCounter(self.category, self.counter, self.process)
             cycles = int(self.runTime/self.interval)
             
             for x in range(cycles):  
                 print pc.NextValue()
-                self.parent.progressBarCycles.setValue(int(x/cycles)) # Update progress bar!
+                self.progressBarCycles.setValue( int( 100*(x/cycles) ) ) # Update progress bar!
                 sleep(self.interval)
-                
-                if self.halt:
-                    print "Thread HALTING!"
-                    break
                 
         except InvalidOperationException:
             print "Could not find process \"%(process)s\"!" % {'process':self.process}
 
-        self.parent.progressBarCycles.hide()
-        
+        self.progressBarCycles.hide()
         
         
     def fromOptions(self):
-        self.category = str(self.parent.comboBoxCategory.currentText())
-        self.counter = str(self.parent.comboBoxCounter.currentText())
-        self.process = str(self.parent.lineEditProcess.text())
+        self.category = str(self.comboBoxCategory.currentText())
+        self.counter = str(self.comboBoxCounter.currentText())
+        self.process = str(self.lineEditProcess.text())
         
-        self.interval = self.parent.doubleSpinBoxInterval.value()
-        self.runTime = self.parent.spinBoxRunTime.value()
+        self.interval = self.doubleSpinBoxInterval.value()
+        self.runTime = self.spinBoxRunTime.value()
              
         
     
