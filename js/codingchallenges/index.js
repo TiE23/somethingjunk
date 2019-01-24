43109e2821e5b5f207b013398c3db83e614e0b44
Kyle Geib <kyle.geib@gmail.com>
2019-01-23 23:03:23 -0800

    Second chapter of questions "Linked Lists" started.
    Linked lists! It's probably been 7 or maybe even 8 years since I wrote linked lists!
    
    Got the first two questions done including a second variation of an answer to the first question. Might do some more tomorrow or move onto the next chapter.
    
    I was thinking LinkedLists would be dumb but actually it taught me simple ES6 classes. When you spend so much time doing complicated code you kinda lose touch with the basics of the language so taking the chance to just write some real simple, nuts & bolts code is really refreshing... and will prevent huge embarrassments for me during a challenge.
    
    In that... I recall in my first technical interview forgetting the essential Java language function .equals() completely which is like taking a driving test and forgetting what part the key plays in driving your car.

--- js/codingchallenges/index.js
+++ js/codingchallenges/index.js
@@ -1,6 +1,8 @@
 /* eslint-disable no-console, camelcase */
 
-// 1.1 ////////////////////////////////////////////////////////////////////////////////////////////
+// /////////////////////////////////////////////////////////////////////////////////////////////////
+// Chapter 1 - Arrays and Strings
+// 1.1 /////////////////////////////////////////////////////////////////////////////////////////////
 // Implement an algorithm to determine if a string has all unique characters. What if you cannot use
 // additional data structures?
 const q1_1 = (input) => {
@@ -26,7 +28,7 @@ const q1_1 = (input) => {
 // console.log("aa", q1_1("aa"));
 
 
-// 1.3 ////////////////////////////////////////////////////////////////////////////////////////////
+// 1.3 /////////////////////////////////////////////////////////////////////////////////////////////
 // Given two strings, write a method to decide if one is a permutation of the other.
 function q1_3(left, right) {
   if (left.length !== right.length) return false;
@@ -68,7 +70,7 @@ function q1_3(left, right) {
 // console.log("listen please, silent please", q1_3("listen please", "silent please"));
 
 
-// 1.4 ////////////////////////////////////////////////////////////////////////////////////////////
+// 1.4 /////////////////////////////////////////////////////////////////////////////////////////////
 // Write a method to replace ALL spaces with "%20. Ex: "Hello Mr    Doe     " -> "Hello%20Mr%20Doe"
 function q1_4(input) {
   // return input.trim().replace(/ +/g, "%20");
@@ -104,7 +106,7 @@ function q1_4(input) {
 // console.log("Hello world", "->", q1_4("Hello world"));
 
 
-// 1.5 ////////////////////////////////////////////////////////////////////////////////////////////
+// 1.5 /////////////////////////////////////////////////////////////////////////////////////////////
 // Write a simple algorithm to "compress" a string. For example, "aabbbcddd" would become "a2b3c1d3"
 // If the "compressed" string is longer, just return the original.
 function q1_5(input) {
@@ -140,7 +142,7 @@ function q1_5(input) {
 // console.log("aabb", q1_5("aabb"));
 
 
-// 1.6 ////////////////////////////////////////////////////////////////////////////////////////////
+// 1.6 /////////////////////////////////////////////////////////////////////////////////////////////
 // Given an image represented by an NxN matrix, where each pixel is the image is 4 bytes, write a
 // method to rotate the image by 90 degrees. Can you do it in place?
 // Qs: Can I simply use a 2-dimensional array with string characters?
@@ -218,7 +220,7 @@ const img4x4 = [
 // console.log(img4x4, q1_6b(img4x4));
 
 
-// 1.7 ////////////////////////////////////////////////////////////////////////////////////////////
+// 1.7 /////////////////////////////////////////////////////////////////////////////////////////////
 // Write an algorithm such that if in an element in an MxN matrix is 0, its entire row and column
 // are set to 0.
 function q1_7(input) {
@@ -270,7 +272,7 @@ const matrixB = [
 // console.log(matrixA, q1_7(matrixA));
 // console.log(matrixB, q1_7(matrixB));
 
-// 1.8 ////////////////////////////////////////////////////////////////////////////////////////////
+// 1.8 /////////////////////////////////////////////////////////////////////////////////////////////
 // Assume you have a method isSubstring which checks if one word is a substring of another. Given
 // two strings, s1 and s2, write code to check if s2 is a rotation of s1 using only one call to
 // isSubstring (e.g., "waterbottle is a rotation of "erbottlewat")
@@ -285,4 +287,248 @@ function q1_8(needle, haystack) {
   // are the same length before doing the rest of the function.
 }
 
-console.log("waterbottle", "erbottlewat", q1_8("waterbottle", "erbottlewat"));
+// console.log("waterbottle", "erbottlewat", q1_8("waterbottle", "erbottlewat"));
+
+
+// /////////////////////////////////////////////////////////////////////////////////////////////////
+// Chapter 2 - Linked Lists
+class Node {
+  constructor(data, next = null) {
+    this.data = data;
+    this.next = next;
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+  }
+
+  insertAtBeginning(data) {
+    const newNode = new Node(data);
+    newNode.next = this.head;
+    this.head = newNode;
+
+    return this.head;
+  }
+
+  insertAtEnd(data) {
+    const newNode = new Node(data);
+
+    if (!this.head) {
+      this.head = newNode;
+      return this.head;
+    } else {
+      let tail = this.head;
+
+      while (tail.next !== null) {
+        tail = tail.next;
+      }
+      tail.next = newNode;
+    }
+
+    return this.head;
+  }
+
+  deleteNodeByData(targetData, firstNode = undefined) {
+    if (firstNode === null) {
+      return true;
+    }
+
+    if (!firstNode && this.head.data === targetData) {
+      this.head = this.head.next;
+      return true;
+    }
+
+    let currentNode = firstNode || this.head;
+
+    while (currentNode.next !== null) {
+      if (currentNode.next.data === targetData) {
+        currentNode.next = currentNode.next.next;
+        return true;
+      } else {
+        currentNode = currentNode.next;
+      }
+    }
+
+    return false;
+  }
+
+  toString() {
+    const data = [];
+    let currentNode = this.head;
+
+    while (currentNode !== null) {
+      data.push(currentNode.data);
+      currentNode = currentNode.next;
+    }
+
+    return data.join(", ");
+  }
+}
+
+// const list = new LinkedList();
+// list.insertAtEnd("Hello");
+// list.insertAtEnd("World!");
+// list.insertAtBeginning("Why");
+// console.log(String(list));
+//
+// list.deleteNodeByData("World!");
+// console.log(String(list));
+// list.deleteNodeByData("Why");
+// console.log(String(list));
+// list.deleteNodeByData("Hello");
+// console.log(String(list));
+
+// Example of extending with a new function for one-time use.
+class LinkedListExtensionExample extends LinkedList {
+  printHeadData() {
+    return `Head Data: ${this.head.data}`;
+  }
+}
+
+// const extendedExampleList = new LinkedListExtensionExample();
+// extendedExampleList.insertAtEnd("Hey");
+// extendedExampleList.insertAtEnd("there");
+// console.log(extendedExampleList.printHeadData());
+
+// 2.1 /////////////////////////////////////////////////////////////////////////////////////////////
+// Write code to remove duplicates from an unsorted linked list.
+// Follow up: How would you solve this problem if a temporary buffer is not allowed?
+
+// Using a modified delete function
+class LinkedList2_1a extends LinkedList {
+  removeDupes() {
+    if (this.head === null || this.head.next === null) {
+      return true; // No dupes possible if empty or length = 1
+    }
+
+    let currentNode = this.head;
+
+    while (currentNode.next !== null) {
+      let deDuped = false;
+
+      // Delete all duplicates of the currentNode starting with the next.
+      do {
+        deDuped = !this.deleteNodeByData(currentNode.data, currentNode.next);
+      } while (!deDuped);
+
+      // Onto the next node.
+      currentNode = currentNode.next;
+    }
+
+    return true;
+  }
+}
+
+// Using a simple runner style (repeats deletion code).
+class LinkedList2_1b extends LinkedList {
+  removeDupes() {
+    if (this.head === null || this.head.next === null) {
+      return true; // No dupes possible if empty of length = 1
+    }
+
+    let checkedNode = this.head;
+
+    while (checkedNode !== null) {
+      let currentNode = checkedNode;
+
+      while (currentNode.next !== null) {
+        if (currentNode.next.data === checkedNode.data) {
+          currentNode.next = currentNode.next.next;
+        } else {
+          currentNode = currentNode.next;
+        }
+      }
+      checkedNode = checkedNode.next;
+    }
+
+    return true;
+  }
+}
+
+// const list2_1 = new LinkedList2_1b();
+// list2_1.insertAtEnd("A");
+// list2_1.insertAtEnd("B");
+// list2_1.insertAtEnd("C");
+// list2_1.insertAtEnd("A");
+// list2_1.insertAtEnd("B");
+// console.log("Before:", String(list2_1)); // Before: A, B, C, A, B
+// list2_1.removeDupes();
+// console.log("After: ", String(list2_1)); // After:  A, B, C
+// list2_1.insertAtEnd("B");
+// console.log("Before:", String(list2_1)); // Before: A, B, C, B
+// list2_1.removeDupes();
+// console.log("After: ", String(list2_1)); // After:  A, B, C
+// list2_1.removeDupes();
+// console.log("After: ", String(list2_1)); // After:  A, B, C
+// list2_1.insertAtEnd("B");
+// console.log("Before:", String(list2_1)); // Before: A, B, C, B
+// list2_1.removeDupes();
+// console.log("After: ", String(list2_1)); // After:  A, B, C
+// list2_1.removeDupes();
+// console.log("After: ", String(list2_1)); // After:  A, B, C
+
+
+// 2.2 /////////////////////////////////////////////////////////////////////////////////////////////
+// Implement an algorithm to find the kth to last element of a single linked list.
+// 0 would be the last element. Don't delete anything if rIndex is too high.
+class LinkedList2_2 extends LinkedList {
+  deleteFromLast(rIndex) {
+    if (rIndex < 0) return false; // Don't allow negative numbers
+
+    const traverse = (currentNode, targetrIndex) => {
+      let lastness;
+
+      if (currentNode.next === null) {  // Found the tail
+        return 0;
+      } else {
+        lastness = traverse(currentNode.next, targetrIndex);  // Go deeper
+      }
+
+      if (lastness === targetrIndex) {  // Base case
+        currentNode.next = currentNode.next.next;
+        return true;
+      }
+
+      if (lastness !== true) {
+        return lastness + 1;
+      } else {
+        return true;
+      }
+    };
+
+    const result = traverse(this.head, rIndex);
+
+    // In the case the target is the head we gotta do this check on the final returned value.
+    if (result === rIndex) {
+      this.head = this.head.next;
+      return true;
+    } else {
+      // Otherwise return the traverse result. True is it was removed.
+      return result === true;
+    }
+  }
+}
+
+// const list2_2 = new LinkedList2_2();
+// list2_2.insertAtEnd("A");
+// list2_2.insertAtEnd("B");
+// list2_2.insertAtEnd("C");
+// list2_2.insertAtEnd("D");
+// list2_2.insertAtEnd("E");
+// console.log("Before:", String(list2_2)); // Before: A, B, C, D, E
+// list2_2.deleteFromLast(0);
+// console.log("After: ", String(list2_2)); // After:  A, B, C, D
+// list2_2.deleteFromLast(1);
+// console.log("After: ", String(list2_2)); // After:  A, B, D
+// list2_2.deleteFromLast(9);
+// console.log("After: ", String(list2_2)); // After:  A, B, D
+// list2_2.deleteFromLast(-1);
+// console.log("After: ", String(list2_2)); // After:  A, B, D
+// list2_2.deleteFromLast(2);
+// console.log("After: ", String(list2_2)); // After:  B, D
+// list2_2.deleteFromLast(0);
+// console.log("After: ", String(list2_2)); // After:  B
+// list2_2.deleteFromLast(0);
+// console.log("After: ", String(list2_2)); // After:  (Blank)
