01ce7e5b9763dc8fcc0f4a82b6bfb81d8f3dd0e6
Kyle Geib <kyle.geib@gmail.com>
2022-04-11 18:37:15 -0400

    RestAPI-Practice - Connecting to the MongoDB server. Adding basic logging as well using Pino. Had to make a change to avoid a deprecated solution with pino-pretty.

--- typescript/RestAPI/src/app.ts
+++ typescript/RestAPI/src/app.ts
@@ -1,11 +1,14 @@
-import { toUpperCase } from "./utils/strings";
 import express from "express";
+import config from "config";
 
+import connect from "./utils/connect";
+import logger from "./utils/logger";
+
+const port = config.get<number>("port");
 const app = express();
-app.listen(1337, () => {
-  console.log("App is running");
+
+app.listen(port, async () => {
+  logger.info(`App is running at http://localhost:${port}`);
+
+  await connect();
 });
-
-const message = "Hello world!";
-
-console.log(toUpperCase(message));
