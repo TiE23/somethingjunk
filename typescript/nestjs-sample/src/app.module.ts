a4667431615bf42510d6e6b42f9ebaf6605ebf22
Kyle Geib <kyle.geib@gmail.com>
2022-04-27 15:54:22 -0400

    NestJS-Sample - Hooking up the mongo server through Mongoose and NestJS Modules.

--- typescript/nestjs-sample/src/app.module.ts
+++ typescript/nestjs-sample/src/app.module.ts
@@ -2,9 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ItemsModule } from './items/items.module';
+import { MongooseModule } from '@nestjs/mongoose';
+
+import keys from './config/keys';
 
 @Module({
-  imports: [ItemsModule],
+  imports: [ItemsModule, MongooseModule.forRoot(keys.mongoUri)],
   controllers: [AppController],
   providers: [AppService],
 })
