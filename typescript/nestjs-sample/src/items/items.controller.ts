96d4b2d67424005447ed5d487bfc735434530c21
Kyle Geib <kyle.geib@gmail.com>
2022-04-27 12:27:06 -0400

    NestJS-Sample - New GET route with a param option for :id. Also wrote out a simple example of accessing the raw request and response like we do in Express.

--- typescript/nestjs-sample/src/items/items.controller.ts
+++ typescript/nestjs-sample/src/items/items.controller.ts
@@ -1,4 +1,6 @@
-import { Controller, Get, Post, Body } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param } from '@nestjs/common';
+// import { Req, Res } from '@nestjs/common';
+// import { Request, Response } from 'express';
 
 import { CreateItemDto } from './dto/create-item.dto';
 
@@ -9,6 +11,20 @@ export class ItemsController {
   findAll(): string {
     return 'Get all items';
   }
+
+  // Example of using route params. Here we have :id and retrieve it with another decorator.
+  @Get(':id')
+  findOne(@Param() param): string {
+    return `Item ${param.id}`;
+  }
+
+  // "This is not really the NestJS way to do this..."
+  // This is an example of how I would get the res and req of the route for express.
+  // @Get()
+  // findAll(@Req() req: Request, @Res() res: Response): Response {
+  //   console.log(req.url);
+  //   return res.send('hello world!!');
+  // }
 
   // Using the DTO we now get to define the Body of the post request as taking
   // the form of the DTO
